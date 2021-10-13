3fa9a37874030dccc1a180e74fba5fcf53dedc90
JANFAN-L <janfanlu@gmail.com>
2021-10-12 23:40:44 -0400

    10/12 01

---
+++ Source/FPSBasketball/FPSBasketball.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FPSBasketball.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPSBasketball, "FPSBasketball" );
---
+++ Source/FPSBasketball/FPSBasketballGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSBasketballGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSBASKETBALL_API AFPSBasketballGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
